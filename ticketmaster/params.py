2937d460d2da589244e4eae8dafedf1fbe146986
Sajjan <sajjanpaudel811@gmail.com>
2024-10-08 12:38:54 +0545

    runOnStartup to False

--- ticketmaster/params.py
+++ ticketmaster/params.py
@@ -1,11 +1,11 @@
 # production
-params = {
-    "dbname": "salesdb1",
-    "user": "salespredictionsql",
-    "password": "Shajir86@ms9",
-    "host": "sales-prediction-svr-v2.postgres.database.azure.com",
-    "port": "5432",
-}
+# params = {
+#     "dbname": "salesdb1",
+#     "user": "salespredictionsql",
+#     "password": "Shajir86@ms9",
+#     "host": "sales-prediction-svr-v2.postgres.database.azure.com",
+#     "port": "5432",
+# }
 # staging
 # params = {
 #     'dbname': 'salesdb1',
@@ -16,10 +16,10 @@ params = {
 # }
 
 # local
-# params = {
-#     'dbname': 'salesdb',
-#     'user': 'postgres',
-#     'password': '',
-#     'host': 'localhost',
-#     'port': '5432'
-# }
+params = {
+    'dbname': 'salesdb',
+    'user': 'postgres',
+    'password': '',
+    'host': 'localhost',
+    'port': '5432'
+}
